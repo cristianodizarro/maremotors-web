1bf6c10af6e51cce7c450f205097bccd16d84360
Cristiano Dizarro <cridizarro@hotmail.com>
2021-01-27 01:45:04 -0300

    Copiando os arquivos do diretório do desktop

---
+++ js/script_pagina_inicial.js
@@ -0,0 +1,26 @@
+
+//Direcionamentos da página principal --> links do topo da página
+
+//Direcionamento para a página inicial
+var paginaInicial = document.getElementById('paginaInicial');
+	paginaInicial.addEventListener('click', function(){
+		window.location.href = 'file:///C:/Users/Cristiano/Documents/Site%20Mare%20Motos/index.html#'
+	})
+	
+//Direcionamento para a página de serviços
+var paginaInicial = document.getElementById('paginaServico');
+	paginaServico.addEventListener('click', function(){
+		window.location.href = 'https://www.google.com/search?ei=xw6MX675I_yy5OUPoKCzyAY&q=tradutor&oq=tra&gs_lcp=CgZwc3ktYWIQAxgBMgQIABBDMgQIABBDMgQIABBDMgQIABBDMgQIABBDMgIIADICCC4yAgguMgIIADICCABQshlYjRxgtytoAHAAeACAAXWIAc0CkgEDMC4zmAEEoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab'
+	})
+	
+//Direcionamento para a página de anuncio de motos
+var paginaInicial = document.getElementById('paginaMotos');
+	paginaInicial.addEventListener('click', function(){
+		window.location.href = 'file:///C:/Users/Cristiano/Documents/Site%20Mare%20Motos/index.html#'
+	})
+	
+//Direcionamento para a página de contato
+var paginaInicial = document.getElementById('paginaContato');
+	paginaInicial.addEventListener('click', function(){
+		window.location.href = 'file:///C:/Users/Cristiano/Documents/Site%20Mare%20Motos/index.html#'
+	})
